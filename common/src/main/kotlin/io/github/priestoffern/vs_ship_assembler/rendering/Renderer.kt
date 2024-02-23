e5684e88f34753cdf93863fa80001ebe181f5f28
PriestOfFerns <timofeydenin@gmail.com>
2024-02-23 15:05:23 +0300

    :bug: Fix java.util.ConcurrentModificationException Crash

--- common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
+++ common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
@@ -2,6 +2,7 @@ package io.github.priestoffern.vs_ship_assembler.rendering
 
 import com.google.common.base.Supplier
 import com.mojang.blaze3d.vertex.PoseStack
+import dev.architectury.event.events.client.ClientTooltipEvent.Render
 
 import net.minecraft.client.Camera
 import net.minecraft.client.Minecraft
@@ -10,17 +11,23 @@ import org.valkyrienskies.mod.common.shipObjectWorld
 
 
 fun renderData(poseStack: PoseStack, camera: Camera) {
+    if (Renderer.rendering) return // Sometimes renderData() seems to go off before removeAll() is done, causing a crash. No clue why that happens though?
+    Renderer.rendering = true;
     for (data in Renderer.toRender) {
+
         data.renderData(poseStack, camera)
 
     }
     Renderer.toRender.removeAll(Renderer.toRemove)
+    Renderer.rendering = false;
 }
 
 object Renderer {
     var CurrentId:Long = 0;
     val toRender = mutableListOf<RenderingData>()
     val toRemove = mutableListOf<RenderingData>()
+
+    var rendering = false;
     fun addRender(renderData: RenderingData): RenderingData {
         renderData.Id = CurrentId;
         CurrentId++
