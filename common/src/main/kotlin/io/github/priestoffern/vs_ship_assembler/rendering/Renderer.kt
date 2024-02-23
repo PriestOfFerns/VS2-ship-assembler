a6dcb87282fd41d15c14abba6ec5739d4574f8bd
PriestOfFerns <timofeydenin@gmail.com>
2024-02-23 17:00:22 +0300

    :bug: fix nullPointer error?

--- common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
+++ common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
@@ -1,21 +1,14 @@
 package io.github.priestoffern.vs_ship_assembler.rendering
 
-import com.google.common.base.Supplier
 import com.mojang.blaze3d.vertex.PoseStack
-import dev.architectury.event.events.client.ClientTooltipEvent.Render
-
 import net.minecraft.client.Camera
-import net.minecraft.client.Minecraft
-
-import org.valkyrienskies.mod.common.shipObjectWorld
 
 
 fun renderData(poseStack: PoseStack, camera: Camera) {
 
     val currentlyRendering = Renderer.toRender.toMutableList() // Have to do this to prevent ConcurrentModificationException
     for (data in currentlyRendering) {
-
-        data.renderData(poseStack, camera)
+        if (data!=null) data.renderData(poseStack, camera)
 
     }
 
